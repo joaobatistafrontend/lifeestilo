a9ff74a3a9cca6e27ba99269e8d1d885c7b018e0
joaobatistafrontend <jbbuno007@gmail.com>
2025-07-11 18:54:33 -0300

    corecao if em imgs

--- life/settings.py
+++ life/settings.py
@@ -136,3 +136,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Certifique-se de ajustar conforme
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGOUT_REDIRECT_URL = 'index'
